e629a4c0d7217714ef20c2f83b29ff6a6e898e39
SerenaWyz <131460262+SerenaWyz@users.noreply.github.com>
2026-05-01 17:31:43 +1200

    Update 1341-Movie-Rating.sql

--- leecode/1341-Movie-Rating.sql
+++ leecode/1341-Movie-Rating.sql
@@ -41,4 +41,19 @@ Output:
 | Frozen 2     |
 +--------------+
 */
-
+--the name of the user who has rated the greatest number of movies.
+(select u.name as results
+from Users u
+join MovieRating mr on u.user_id=mr.user_id
+group by u.user_id, u.name 
+order by count(DISTINCT mr.movie_id)DESC, u.name ASC
+limit 1)
+union all
+--the movie name with the highest average rating in February 2020
+(select m.title as results
+from Movies m
+join MovieRating mr on m.movie_id=mr.movie_id
+where mr.created_at LIKE '2020-02%'
+group by m.movie_id, m.title
+order by AVG(m.rating) DESC, m.title ASC
+limit 1) 
